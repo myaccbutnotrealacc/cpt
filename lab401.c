47fa435eff9449d6731d2c8819fdee431a3fc647
obkeena <pamellpamill@gmail.com>
2018-09-19 13:27:53 +0700

    add code lab401

---
+++ lab401.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void replace_space(char *s, char c);
+
+int main(){
+  char c;
+  char things[128],*s ; 
+  printf("Enter: ");
+   fgets(things,128,stdin);
+  printf("Char: ");
+   scanf(" %c",&c);
+  s = things; 
+  printf("= ");
+  replace_space(s, c);
+ return 0;
+}
+
+void replace_space(char *s, char c){
+  while(*s != '\0'){
+  	if( *s == ' ' ){
+  		printf("%c",c);
+	  } 
+	else
+       printf("%c",*s);
+  *s++;
+  }
+}
+
